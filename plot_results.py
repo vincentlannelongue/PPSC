4a48748b0636f9d23b05d8d4da9db731682ab71c
Vincent Lannelongue <vincent.lannelongue@minesparis.psl.eu>
2026-07-07 10:15:02 +0200

    Finish reduc part

--- plot_results.py
+++ plot_results.py
@@ -13,17 +13,19 @@ def method_0():
     zeta0_path = "zeta0/vtest_results.txt"
     mach0_path = "mach0/vtest_results.txt"
 
-    zeta0 = pd.read_csv(zeta0_path, names=columns, sep=' ')
-    mach0 = pd.read_csv(mach0_path, names=columns, sep=' ')
+    zeta0 = pd.read_csv(zeta0_path, names=columns, sep=" ")
+    mach0 = pd.read_csv(mach0_path, names=columns, sep=" ")
 
     plt.plot(zeta0["k"], zeta0["error"], label="Zeta")
     plt.plot(mach0["k"], mach0["error"], label="Machin")
 
-    plt.xlabel("k", fontsize=14, family='serif')
-    plt.ylabel("Error", fontsize=14, family='serif')
-    plt.xticks(zeta0["k"], )
+    plt.xlabel("k", fontsize=14, family="serif")
+    plt.ylabel("Error", fontsize=14, family="serif")
+    plt.xticks(
+        zeta0["k"],
+    )
     plt.yscale("log")
-    plt.legend(loc='upper right')
+    plt.legend(loc="upper right")
     plt.savefig(os.path.join(RESULTS_PATH, "method_0.svg"))
     plt.close()
 
@@ -32,19 +34,19 @@ def method_1_error(method):
     path = f"{method}1/results.txt"
 
     columns = ["procs", "n", "pi_approx", "error", "time"]
-    df = pd.read_csv(path, names=columns, sep=' ')
+    df = pd.read_csv(path, names=columns, sep=" ")
 
     for procs_value, group in df.groupby("procs"):
         plt.plot(group["n"], group["error"], label=f"{procs_value}")
         plt.scatter(group["n"], group["error"])
     plt.xticks(group["n"])
-    plt.xlabel("n", fontsize=14, family='serif')
-    plt.ylabel("Error", fontsize=14, family='serif')
+    plt.xlabel("n", fontsize=14, family="serif")
+    plt.ylabel("Error", fontsize=14, family="serif")
     plt.yscale("log")
     plt.xscale("log")
 
     plt.title(f"{method} - Error")
-    plt.legend(loc='upper right')
+    plt.legend(loc="upper right")
     plt.savefig(os.path.join(RESULTS_PATH, f"{method}_1_error.svg"))
     plt.close()
 
@@ -53,27 +55,58 @@ def method_1_time(method):
     path = f"{method}1/results.txt"
 
     columns = ["procs", "n", "pi_approx", "error", "time"]
-    df = pd.read_csv(path, names=columns, sep=' ')
+    df = pd.read_csv(path, names=columns, sep=" ")
 
     for procs_value, group in df.groupby("n"):
         plt.plot(group["procs"], group["time"], label=f"{procs_value}")
         plt.scatter(group["procs"], group["time"])
     plt.xticks(group["procs"])
-    plt.xlabel("procs", fontsize=14, family='serif')
-    plt.ylabel("Time (s)", fontsize=14, family='serif')
+    plt.xlabel("procs", fontsize=14, family="serif")
+    plt.ylabel("Time (s)", fontsize=14, family="serif")
     plt.yscale("log")
     plt.xscale("log")
     plt.title(f"{method} - Time")
 
-    plt.legend(loc='upper right')
+    plt.legend(loc="upper right")
     plt.savefig(os.path.join(RESULTS_PATH, f"{method}_1_time.svg"))
     plt.close()
 
 
-if __name__ == '__main__':
+def reduc():
+    for method, linestyle in zip(
+        ["reduc", "doubling"], ["-", "-."]
+    ):
+
+        path = f"reduc/results_{method}.txt"
+        columns = ["procs", "n", "pi_approx", "error", "time"]
+        df = pd.read_csv(path, names=columns, sep=" ")
+
+        for colour, (procs_value, group) in zip(["red", "blue", "green", "brown"], df.groupby("n")):
+            plt.plot(
+                group["procs"],
+                group["time"],
+                label=f"{method}-{procs_value}",
+                linestyle=linestyle,
+                color=colour,
+            )
+            # plt.scatter(group["procs"], group["time"], color=colour)
+    plt.xticks(group["procs"])
+    plt.xlabel("procs", fontsize=14, family="serif")
+    plt.ylabel("Time (s)", fontsize=14, family="serif")
+    plt.yscale("log")
+    plt.xscale("log")
+    plt.title("Reduction method comparison")
+
+    plt.legend(loc="upper right")
+    plt.savefig(os.path.join(RESULTS_PATH, f"reduc_time.svg"))
+    plt.close()
+
+
+if __name__ == "__main__":
     os.makedirs(RESULTS_PATH, exist_ok=True)
     method_0()
     method_1_error("zeta")
     method_1_error("mach")
     method_1_time("zeta")
     method_1_time("mach")
+    reduc()
---
+++ reduc/job.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#SBATCH --job-name=PPSC
+#SBATCH --output=out.log
+#
+#SBATCH --nodes 1
+#SBATCH --ntasks 64
+#SBATCH --ntasks-per-node=64
+#SBATCH --ntasks-per-core=1
+#SBATCH --partition=CPU
+#SBATCH --qos=normal
+# SBATCH --exclude='node-101,node-102,node-103,node-104,node-105,node-106,node-120'
+
+# Load module
+module load cimlibxx
+
+# Set environment variable for OpenMP
+export OMP_PROC_BIND=true
+export OMPI_MCA_btl=^openib
+
+make comp
+
+echo "Running zeta1..."
+
+for n in 1 1000 1000000 1000000000
+do
+    for procs in 1 2 4 8 16 32
+    do
+        echo "Running with n=${n} and np=${procs}..."
+        mpirun -n ${procs} ./main ${n}
+    done
+done
