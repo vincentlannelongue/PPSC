ca5b411b4725ef794c550fe9cd386e7b4cc45e5d
Vincent Lannelongue <vincent.lannelongue@minesparis.psl.eu>
2026-07-07 08:19:53 +0200

    Complete method 1 plots

--- plot_results.py
+++ plot_results.py
@@ -1,6 +1,3 @@
-
-
-
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -31,7 +28,52 @@ def method_0():
     plt.close()
 
 
+def method_1_error(method):
+    path = f"{method}1/results.txt"
+
+    columns = ["procs", "n", "pi_approx", "error", "time"]
+    df = pd.read_csv(path, names=columns, sep=' ')
+
+    for procs_value, group in df.groupby("procs"):
+        plt.plot(group["n"], group["error"], label=f"{procs_value}")
+        plt.scatter(group["n"], group["error"])
+    plt.xticks(group["n"])
+    plt.xlabel("n", fontsize=14, family='serif')
+    plt.ylabel("Error", fontsize=14, family='serif')
+    plt.yscale("log")
+    plt.xscale("log")
+
+    plt.title(f"{method} - Error")
+    plt.legend(loc='upper right')
+    plt.savefig(os.path.join(RESULTS_PATH, f"{method}_1_error.svg"))
+    plt.close()
+
+
+def method_1_time(method):
+    path = f"{method}1/results.txt"
+
+    columns = ["procs", "n", "pi_approx", "error", "time"]
+    df = pd.read_csv(path, names=columns, sep=' ')
+
+    for procs_value, group in df.groupby("n"):
+        plt.plot(group["procs"], group["time"], label=f"{procs_value}")
+        plt.scatter(group["procs"], group["time"])
+    plt.xticks(group["procs"])
+    plt.xlabel("procs", fontsize=14, family='serif')
+    plt.ylabel("Time (s)", fontsize=14, family='serif')
+    plt.yscale("log")
+    plt.xscale("log")
+    plt.title(f"{method} - Time")
+
+    plt.legend(loc='upper right')
+    plt.savefig(os.path.join(RESULTS_PATH, f"{method}_1_time.svg"))
+    plt.close()
+
+
 if __name__ == '__main__':
     os.makedirs(RESULTS_PATH, exist_ok=True)
     method_0()
-    
+    method_1_error("zeta")
+    method_1_error("mach")
+    method_1_time("zeta")
+    method_1_time("mach")
